458992b5875e410fea9e908c9a4f057303066595
Gerard Mortel <gmortel@us.ibm.com>
2023-06-05 17:53:23 -0500

    Fixed service account name with patch file

--- 07_create_phpldapadmin_app.sh
+++ 07_create_phpldapadmin_app.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+
 # Create the phpldapadmin app
 oc new-app --name phpldapadmin --as-deployment-config -e PHPLDAPADMIN_LDAP_HOSTS=openldap ${PHPLDAPADMINIMAGESTREAM}
-oc patch dc/phpldapadmin -p '{"spec":{"strategy":{"type":"Recreate"},"template":{"spec":{"serviceAccountName":"${SERVICEACCOUNTNAME}"}}}}'
+
+# Patch the app with a Recreate strategy to stop the constant pod killing
+#oc patch dc/phpldapadmin -p '{"spec":{"strategy":{"type":"Recreate"},"template":{"spec":{"serviceAccountName":"${SERVICEACCOUNTNAME}"}}}}'
+cat << EOF > new_dc_phpldapadmin_patch.yaml
+spec:
+  strategy:
+    type: Recreate
+  template:
+    spec:
+      serviceAccountName: ${SERVICEACCOUNTNAME}
+EOF
+oc patch dc/openldap --patch-file='new_dc_phpldapadmin_patch.yaml' --type='merge'
+
 oc expose service/phpldapadmin

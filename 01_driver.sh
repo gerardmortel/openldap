f28589a2fbbeca8fcf3e87af1e5fc4557c7c7e09
Gerard Mortel <gmortel@us.ibm.com>
2023-06-05 17:00:05 -0500

    Renamed files

--- 01_driver.sh
+++ 01_driver.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# For first time run, run steps 2 thru 6
+# For first time run, run steps 2 thru 7
 . ./02_setup_env.sh
 ./03_build_openldap_image.sh
-./04_create_openldap_app.sh
-./05_build_phpldapadmin_image.sh
-./06_create_phpldapadmin_app.sh
+04_push_openldap_image.sh
+05_create_openldap_app.sh
+06_build_phpldapadmin_image.sh
+07_create_phpldapadmin_app.sh
 
-# Subsequent runs do not need to rebuild images.  Just run steps 2, 4 and 6.
+
+# Subsequent runs do not need to rebuild images.  Comment out steps 3 and 5, run all the rest
 # . ./02_setup_env.sh
-# ./03_build_openldap_image.sh
-# ./04_create_openldap_app.sh
-# ./05_build_phpldapadmin_image.sh
-# ./06_create_phpldapadmin_app.sh
+# #./03_build_openldap_image.sh
+# 04_push_openldap_image.sh
+# #05_create_openldap_app.sh
+# 06_build_phpldapadmin_image.sh
+# 07_create_phpldapadmin_app.sh

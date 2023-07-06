b580621b4650b32bb40d9faf3054977b7d41748f
Gerard Mortel <gmortel@us.ibm.com>
2023-07-05 22:12:49 -0500

    Improved loogic to get OCP image registry

--- 02_setup_env.sh
+++ 02_setup_env.sh
@@ -15,8 +15,20 @@ do
     oc registry info --public
     if [ $? -eq 0 ]; then
         echo "Public route for OpenShift cluster registry IS available."
-        sleep 30
-        export OCPREGISTRYROUTE=$(oc registry info --public)
+        # While loop to successfully export value of OCP image registry
+        while [ true ]
+        do
+            export OCPREGISTRYROUTE=$(oc registry info --public)
+            if [[ $OCPREGISTRYROUTE == *"default"* ]]; then
+                echo "Successfully obtained the OCP image registry URL"
+                echo "OCP image registry URL is: [${OCPREGISTRYROUTE}]"
+                break
+            else
+                echo "Could NOT obtain the OCP image registry URL"
+                echo "Sleeping for 10 seconds"
+                sleep 10
+            fi
+        done
         break
     else
         echo "Public route for OpenShift cluster registry is NOT available."
